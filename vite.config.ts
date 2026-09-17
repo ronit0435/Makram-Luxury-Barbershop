c167df0526b9963c48239d94c1ff8b9f1d9b37da
Ronit <r62544400@gmail.com>
2026-09-17 23:16:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Makram-Luxury-Barbershop/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
